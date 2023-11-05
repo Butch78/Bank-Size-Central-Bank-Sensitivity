86b4cd997ccc359e113e77dc335f15c638cae112
Butch78 <matthewtyleraylward@hotmail.com>
2023-11-05 20:15:03 +0000

    feat: Working Startup

--- app/main.py
+++ app/main.py
@@ -1,41 +1,31 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
 
 from app.utils.config import settings
-from app.utils.deps import create_db_and_tables
+from app.utils.deps import create_db_and_tables, import_data
 from app.api.api import api_router
 
 app = FastAPI()
 
 
-app.on_event("startup")
-async def startup_event():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db_and_tables()
+    import_data()
+    yield
 
 
-@app.on_event("shutdown")
-async def on_shutdown():
-    pass
-
-
-def get_application():
-    _app = FastAPI(title=settings.PROJECT_NAME)
-
-    _app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    _app.include_router(api_router)
-
-    return _app
-
-
-app = get_application()
+app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
+app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
